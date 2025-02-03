02a2c6814e01fe03bade3065a4d8eff6807492e6
Pierce <pbalworth@gmail.com>
2025-02-02 18:02:36 -0700

    routes.ts

---
+++ config/routes.ts
@@ -0,0 +1,19 @@
+export const routes = {
+    home: "/",
+    explore: "/explore",
+    profile: "/profile",
+    behavior: "/behavior",
+    exercise: "/exercise",
+    diet: "/diet",
+    wellness: "/wellness",
+    health: "/health",
+    courses: {
+      meditation: "/courses/meditation",
+      stress: "/courses/stress",
+      emotions: "/courses/emotions",
+    },
+  } as const
+  
+  export type RouteKey = keyof typeof routes
+  
+  
