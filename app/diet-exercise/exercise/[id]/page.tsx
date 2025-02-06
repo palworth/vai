db7166d0eed8cf2369179418dc35786cccca4690
Pierce <pbalworth@gmail.com>
2025-02-05 17:12:02 -0700

    got rid of old exercise ui

--- app/diet-exercise/exercise/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-import { ExerciseEventDetails } from "@/app/components/ExerciseEventDetails"
-
-export default async function ExerciseEventPage({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params
-  return <ExerciseEventDetails id={id} />
-}
-
-
