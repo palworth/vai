80649f9c40a44519528e3949e093bfb9d9eee9c3
Pierce <pbalworth@gmail.com>
2025-02-20 10:00:31 -0700

    added all 5

---
+++ constants/vet-events.ts
@@ -0,0 +1,28 @@
+export const vetEvents = [
+    {
+      title: "Vet Appointment",
+      backgroundColor: "#4169E1", // Royal Blue
+      href: "vet-appointment",
+    },
+    {
+      title: "Vaccination Appointment",
+      backgroundColor: "#32CD32", // Lime Green
+      href: "vaccination-appointment",
+    },
+    {
+      title: "Weight Change",
+      backgroundColor: "#FF4500", // Orange Red
+      href: "weight-change",
+    },
+    {
+      title: "Poop Journal",
+      backgroundColor: "#8B4513", // Saddle Brown
+      href: "poop-journal",
+    },
+    {
+      title: "Health",
+      backgroundColor: "#DC143C", // Crimson
+      href: "health",
+    },
+  ];
+  
