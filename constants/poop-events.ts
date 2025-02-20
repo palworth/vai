deb93d40e160c68566e7bbe3422ebd05155aa4af
Pierce <pbalworth@gmail.com>
2025-02-19 21:15:26 -0700

    routes and constants for poop

---
+++ constants/poop-events.ts
@@ -0,0 +1,9 @@
+// File: /constants/poop-events.ts
+export const poopEvents = [
+    {
+      title: "Poop Journal",
+      backgroundColor: "#8B4513",
+      href: "poop-journal",
+    },
+  ];
+  
